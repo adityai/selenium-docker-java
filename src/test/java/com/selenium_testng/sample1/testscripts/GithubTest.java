5c797a896fdaf7eba60ddb90d022ffedbcca7fa4
Aditya Inapurapu <adityaii@gmail.com>
2020-11-25 22:06:23 -0800

    Remote webdriver in comments

--- src/test/java/com/selenium_testng/sample1/testscripts/GithubTest.java
+++ src/test/java/com/selenium_testng/sample1/testscripts/GithubTest.java
@@ -2,10 +2,15 @@ package com.selenium_testng.sample1.testscripts;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
@@ -27,11 +32,17 @@ public class GithubTest {
 
     @BeforeClass
     public void setup() {
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--headless");
-		DesiredCapabilities desiredCapabilitiesChrome = DesiredCapabilities.htmlUnit();
-		desiredCapabilitiesChrome.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
-		driver = new ChromeDriver(desiredCapabilitiesChrome);
+    	ChromeOptions chromeOptions = new ChromeOptions();
+    	chromeOptions.addArguments("--headless");
+		DesiredCapabilities capability = DesiredCapabilities.chrome();
+		capability.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
+		driver = new ChromeDriver(capability);
+//		try {
+//			driver = new RemoteWebDriver(new URL("http://0.0.0.0:5566/wd/hub"), capability);
+//		} catch (MalformedURLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
     }
 
     @AfterClass
