f53bcba8e045e41bf9cec395d254967df836cf97
Aditya Inapurapu <adityaii@gmail.com>
2020-12-12 14:30:34 -0800

    Adding opera-debug container

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 docker run -d -p 4444:4444 --name selenium-hub selenium/hub
 docker run -d -P --link selenium-hub:hub selenium/node-chrome-debug
 docker run -d -P --link selenium-hub:hub selenium/node-firefox-debug
-docker run -d -P --link selenium-hub:hub selenium/node-firefox-debug
+docker run -d -P --link selenium-hub:hub selenium/node-opera-debug
 docker run -d -P --link selenium-hub:hub selenium/node-firefox
 docker run -d -P --link selenium-hub:hub selenium/node-opera
 docker run -d -P --link selenium-hub:hub selenium/node-chrome
